71ae71724e113eda280e8b5226563b507c2e8ab3
Vedesh <vedesh.a.chavan@gmail.com>
2023-09-13 00:23:46 +0530

    error message

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,15 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = (request, response, next) => {
-    try {
-    const token = request.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY)
+module.exports = async (request, response, next) => {
+  let tokenis = request.headers.authorization;
+  if (!tokenis) {
+    response
+      .status(401)
+      .json({ message: "please provide valid token to access this" });
+  }
+  try {
+    const token = tokenis.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     request.userData = decoded;
     next();
-        
-    } catch (error) {
-      response.status(401).send({
-        message: 'Auth failed'
-      })
-    }
-}
+    
+  } catch (error) {
+    response.status(401).send({
+      message: "Auth failed",
+    });
+  }
+};
--- api/routes/bhoolkus.js
+++ api/routes/bhoolkus.js
@@ -7,6 +7,8 @@ router.get("/", checkAuth, BhoolkusController.bhoolku_get_all);
 
 router.post("/", checkAuth, BhoolkusController.bhoolku_create);
 
+router.post("/mass", checkAuth, BhoolkusController.bhoolku_create_mass);
+
 router.get("/:bhoolkuId", checkAuth, BhoolkusController.bhoolku_get_bhoolku);
 
 router.patch("/:bhoolkuId", checkAuth, BhoolkusController.bhoolku_update);
